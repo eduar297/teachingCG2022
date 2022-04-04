ed2f36cf210c4f1713d1ef443f7f7a4c430006f2
Ludwig Leonard <lleonart1984@gmail.com>
2022-04-05 00:51:05 +0200

    Structure changed.. again.. setup.py to install

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # teachingCG2022
 Collaborative project to develop renderers, scenes and computer graphic techniques.
+
+Install Rendering.
+
+at teachingCG2022
+
+python setup.py install
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Rendering',
+      version='1.0',
+      description='Teaching Computer Graphics',
+      author='Ludwig Leonard',
+      author_email='lleonart1984@gmail.com',
+      url='https://github.com/lleonart1984/teachingCG2022',
+      packages=['rendering'],
+     )
